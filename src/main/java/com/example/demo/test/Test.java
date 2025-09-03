6a359e099f2d10ef55d583a19b52265f23c5f647
gksdn698 <gksdn698@naver.com>
2025-09-03 17:07:15 +0900

    test:jinwoo

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -3,6 +3,7 @@ package com.example.demo.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("08/13 테스트 ");
+        System.out.println("09/03 테스트 ");
+        System.out.println("push태스트");
     }
 }
