624ecddce59edd4ca92d794dd4bb012c00fc270f
류정균 <prank01@naver.com>
2025-09-04 18:41:52 +0900

    pull request:: test

--- src/main/java/com/petscope/test/Test.java
+++ src/main/java/com/petscope/test/Test.java
@@ -4,5 +4,6 @@ package com.petscope.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("08/13 테스트 ");
+        System.out.println(System.getProperty("java.version"));
     }
 }
